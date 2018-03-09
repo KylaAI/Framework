426702d61203e533a19e67be79d06ad9c79b3fb7
Reza Juliandri <rezajuliandri20@yahoo.com>
2018-03-09 21:48:11 +0700

    Update App.php

--- App/Bootstrap/App.php
+++ App/Bootstrap/App.php
@@ -1,47 +1,53 @@
 <?php
 namespace Bootstrap;
+
 /**
-* 
+*  
 */
 class App
 {
-	public static function run(){
-        require_once(BASEPATH.'App/Bootstrap/helper.php');
-        date_default_timezone_set("Asia/Jakarta");
-        self::getClass(
-            self::getSeg(
-                self::check()
-            )
-        );
-	}
-	public static function getClass(String $r){
-        $seg = explode('/',$r);
-        $seg = array_filter($seg);
-        define("SEGMENT",$seg);
-
-        $c = (isset($seg[0]))?$seg[0]:'home';
-        $cU = (isset($seg[0]))?$seg[0]:'';
-        $m = (isset($seg[1]))?$seg[1]:'index';
-        
-        define("CONTROLLER",$cU);
-
-        $c = "\\Http\Controllers\\".ucfirst($c)."Controller";
-        $c = new $c;
-
-        if(!method_exists($c, $m)){
-            die("Method ".$m." Tidak ditemukan");
+    public static function run(){
+        require BASEPATH."App/Bootstrap/helper.php";
+        self::g2(self::g1());
+    }
+    public static function g1(){
+        $s = str_replace("index.php","",$_SERVER['SCRIPT_NAME']);
+        define("BASEURL",$_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST'].$s);
+        return $s;
+    }
+    public static function g2(String $s){
+        if($s == "/"){
+            $req = $_SERVER['REQUEST_URI'];
+        }
+        else {
+            $req = str_replace($s,'',$_SERVER['REQUEST_URI']);
+        }
+        $req = explode("?",$req)[0];
+        $req = explode('/',$req);
+        $req = array_filter($req);
+        $i = 0;
+        $seg = [];
+        foreach ($req as $key) {
+            $seg[$i] = $key;
+            $i++;
         }
 
-        $slice = array_slice($seg,2);
-        call_user_func_array([$c,$m],$slice);
-	}
-	public static function getSeg(String $str){
-		$r = explode($str,$_SERVER['REQUEST_URI'])[1];
-        $r = explode('?',$r)[0];
-        return $r;
-	}
-	public static function check(){
-		$s = str_replace('index.php','',$_SERVER['SCRIPT_NAME']);
-        return $s;
-	}
+        $c = (isset($seg[0]))?$seg[0]:'home';
+        $m = (isset($seg[1]))?$seg[1]:'index';
+
+        $controller = "\\Http\Controllers\\".ucfirst($c)."Controller";
+
+        if(!is_callable([$controller,$m])){
+            die("URL Not Found");
+        }
+        $slice = [];
+        if($seg >2){
+            $slice = array_slice($seg, 2);
+        }
+        try {
+            call_user_func_array([$controller,$m], $slice);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
 }
